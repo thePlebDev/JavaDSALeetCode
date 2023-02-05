f035cb3c6d6e7b32930ba154c5e71505d9ed2c6a
Tristan Elliott <>
2023-02-05 12:54:40 -0400

    basic Singly Linked List

--- app/src/main/java/DSA2/linkedLists/SinglyLinkedList.java
+++ app/src/main/java/DSA2/linkedLists/SinglyLinkedList.java
@@ -50,5 +50,71 @@ public class SinglyLinkedList<E> {
         tail = newNode;
         this.size ++;
     }
+    //END OF THE INITIAL LINKED LIST
+
+    public E removeAt(int position){
+        if(position > -1 && position < size){ // check the bounds
+            Node<E> current = head; // reference to the head
+            Node<E> previous = null; //
+            int index = 0;
+            //1)removing the first item
+            if(position ==0){
+                head = current.next;
+            }else{
+                //2)traverse to the position
+                while (index++ < position){
+                    previous = current;
+                    current = current.getNext();
+                }
+                //link previous with current's nex: skip it to remove
+                previous.setNext(current.next);
+            }
+            size --;
+            return current.getElement();
+        }else{
+            return null;
+        }
+
+    }
+    //INSERT AT
+    public boolean insertAt(int position, E element){
+        //check for boundaries
+        if(position >= 0 && position <= size){
+            Node<E> newNode = new Node(element,null);
+            Node<E> current = head;
+            Node<E> previous = null;
+            int index = 0;
+
+            if(position ==0){ //add on first position
+                newNode.setNext(current);
+                head = newNode;
+
+            }else{
+                while(index++ < position){
+                    previous = current;
+                    current = current.getNext();
+                }
+                newNode.setNext(current);
+                previous.setNext(newNode);
+            }
+            size ++;
+            return true;
+
+        }else{
+            return false;
+        }
+    }
+
+    public String toString(){
+        String startingString = "";
+        Node<E> current = head;
+        while(current != null){
+            startingString += current.getElement() + "---->";
+            current = current.getNext();
+        }
+        return startingString;
+    }
+
+
 
 }
